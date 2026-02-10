1aebb1edd049c74543aef06f3c262c8e28d1a324
Adilyn Rose <arose45@byu.edu>
2026-02-10 16:24:09 -0700

    Added main code to characters and fixed links

--- src/characters/characters.jsx
+++ src/characters/characters.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import './characters.css';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Character_Sheets } from '../character_sheets/character_sheets';
+import { Projects } from '../projects/projects';
 
 export function Characters() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>Characters listed here</div>
+    <main>
+      <div id="fileHeaders"><span><NavLink to="/projects" id="fileLink">Projects</NavLink></span>- Project Name2</div>
+      <div id="characterOrganizer">
+        <div id="Characters"><b>Name</b></div>
+        <div id="Date"><b>Date Modified</b></div>
+      </div>
+      <div id="characterOrganizer">
+        <div id="Characters"><NavLink to="/character_sheets" id="fileLink">John</NavLink></div>
+        <div id="Date">1/26/26</div>
+      </div>
     </main>
   );
 }
