a25d4ba68aff5ebdd7f2b1a57a55986b00fd69a8
Adilyn Rose <arose45@byu.edu>
2026-02-10 16:16:30 -0700

    Added main code to projects.jsx and got the link to characters working

--- src/projects/projects.jsx
+++ src/projects/projects.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import './projects.css';
+import { NavLink } from 'react-router-dom';
+import { Characters } from '../characters/characters';
 
 export function Projects() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>projects listed here</div>
+    <main>
+        <div id="fileHeaders">Projects</div>
+            <div id="projectOrganizer">
+                <div id="Projects"><b>Name</b></div>
+                <div id="Date"><b>Date Modified</b></div>
+                <div id="numCharacters"><b>Characters</b></div>
+            </div>
+            <div id="projectOrganizer">
+                <div id="Projects">Project Name1</div>
+                <div id="Date">1/15/26</div>
+                <div id="numCharacters">8 Charactes</div>
+            </div>
+            <div id="projectOrganizer">
+                <div id="Projects"><NavLink to="/characters" id="fileLink">Project Name2</NavLink></div>
+                <div id="Date">1/26/26</div>
+                <div id="numCharacters">1 Character</div>
+            </div>
     </main>
   );
 }
