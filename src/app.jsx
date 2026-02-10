b57e0410dc5c17ad00bb59b1f0caeb0c6c5b3ad9
Adilyn Rose <arose45@byu.edu>
2026-02-09 22:55:30 -0700

    Created app.jsx and added code

---
+++ src/app.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './app.css';
+
+export default function App() {
+  return <div className="body bg-dark text-light">Will change this later. Following the instructions for Simon</div>;
+}
