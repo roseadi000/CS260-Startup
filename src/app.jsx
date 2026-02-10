1c87d366943d532bc1a866da13bf06855a3ee8de
Adilyn Rose <arose45@byu.edu>
2026-02-09 23:43:46 -0700

    Added router import statements and BrowserRouter component

--- src/app.jsx
+++ src/app.jsx
@@ -1,22 +1,30 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Projects } from './projects/projects';
+import { Characters } from './characters/characters';
+import { Character_Sheets } from './character_sheets/character_sheets';
+import { Friends } from './friends/friends';
 
 export default function App() {
   return (
-    <div className="bodyDisplay">
-        <header>
-            <div id="Logo">Lore Legend</div>
-            <div id="Menu"><a href="friends.html" id="menuLink">Friends</a></div>
-            <div id="User">User</div>
-        </header>
-        
-        <main id="Main">Stuff will go here later</main>
+    <BrowserRouter>
+        <div className="bodyDisplay">
+            <header>
+                <div id="Logo">Lore Legend</div>
+                <div id="Menu"><a href="friends.html" id="menuLink">Friends</a></div>
+                <div id="User">User</div>
+            </header>
+            
+            <main id="Main">Stuff will go here later</main>
 
-        <footer>
-            <div id="myName">Adilyn Rose</div>
-            <a href="https://github.com/roseadi000/CS260-Startup.git" id="gitHub">GitHub</a>
-        </footer>
-    </div>
+            <footer>
+                <div id="myName">Adilyn Rose</div>
+                <a href="https://github.com/roseadi000/CS260-Startup.git" id="gitHub">GitHub</a>
+            </footer>
+        </div>
+    </BrowserRouter>
   );
 }
