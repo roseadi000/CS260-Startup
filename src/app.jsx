125f5cae731359d9f4e237f9839a1d46ea8f3b39
Adilyn Rose <arose45@byu.edu>
2026-02-09 23:29:22 -0700

    Fixed the formatting issue

--- src/app.jsx
+++ src/app.jsx
@@ -4,17 +4,17 @@ import './app.css';
 
 export default function App() {
   return (
-    <div>
+    <div className="bodyDisplay">
         <header>
             <div id="Logo">Lore Legend</div>
         </header>
-
+        
         <main id="Main">Stuff will go here later</main>
 
         <footer>
             <div id="myName">Adilyn Rose</div>
             <a href="https://github.com/roseadi000/CS260-Startup.git" id="gitHub">GitHub</a>
         </footer>
-        </div>
+    </div>
   );
 }
