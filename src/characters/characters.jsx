de3af85ae32aa11f416c3010a5f5b387aaf415e2
Adilyn Rose <arose45@byu.edu>
2026-02-14 13:58:19 -0700

    Got the popup working in characters

--- src/characters/characters.jsx
+++ src/characters/characters.jsx
@@ -3,11 +3,24 @@ import './characters.css';
 import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
 import { Character_Sheets } from '../character_sheets/character_sheets';
 import { Projects } from '../projects/projects';
+import { Popup } from '../scripts';
 
 export function Characters() {
+  const [isPopupOpen, setPopupOpen] = React.useState(false);
+  const [name, setName] = React.useState('Character Name');
+
   return (
     <main>
       <div id="fileHeaders"><span><NavLink to="/projects" id="fileLink">Projects</NavLink></span>- Project Name2</div>
+      <input type='button' value='New Character' onClick={() => setPopupOpen(true)}></input>
+
+      <Popup isOpen={isPopupOpen} onClose={() => setPopupOpen(false)}>
+        <div id='fileHeaders'>Create New Character</div>
+        <input placeholder='Name' onChange={(e) => setName(e.target.value)}></input>
+        <p></p>
+        {/*<input type='button' value='Create' onClick={create}></input>*/}
+      </Popup>
+
       <div id="characterOrganizer">
         <div id="Characters"><b>Name</b></div>
         <div id="Date"><b>Date Modified</b></div>
