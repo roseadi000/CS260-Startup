b42b30138f0f4ef2cbf3fd9688f9d4635d2ce60e
Adilyn Rose <arose45@byu.edu>
2026-02-18 16:33:29 -0700

    Added account sign up popup

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -3,8 +3,10 @@ import './login.css';
 import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
 import { Projects } from '../projects/projects';
 import { registerUser } from '../service.js';
+import { Popup } from '../scripts';
 
 export function Login() {
+  const [isPopupOpen, setPopupOpen] = React.useState(false);
   const [username, setUsername] = React.useState('');
   const [password, setPassword] = React.useState('');
 
@@ -21,7 +23,15 @@ export function Login() {
             <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)}/>
             <p></p>
             <NavLink to="projects">Login</NavLink>
-            <input type='button' value='Sign Up' onClick={register}></input>
+            <input type='button' value='Sign Up' onClick={() => setPopupOpen(true)}></input>
+
+            <Popup isOpen={isPopupOpen} onClose={() => setPopupOpen(false)}>
+              <div id='fileHeaders'>Create New Account</div>
+              <input type="text" placeholder="Username" onChange={(e) => setUsername(e.target.value)}/>
+              <p></p>
+              <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)}/>
+              <input type='button' value='Sign Up' onClick={register}></input>
+            </Popup>
     </main>
   );
 }
