ec68d7ec4cea1b035d3b0d2cd9f4c424ac8d136d
Adilyn Rose <arose45@byu.edu>
2026-02-25 15:20:28 -0700

    Added account information

--- src/account/account.jsx
+++ src/account/account.jsx
@@ -4,9 +4,16 @@ import { Projects } from '../projects/projects';
 import { registerUser, checkLogin } from '../service.js';
 
 export function Account() {
+  const users = JSON.parse(localStorage.getItem('users'));
+  const currentUser = JSON.parse(localStorage.getItem('currentUser'));
+  const user = users.find((u) => u.username === currentUser.username);
+
   return (
-     <main id="Main">
-    <h3>Account</h3>
+    <main>
+      <div id='fileHeaders'>Account</div>
+      <div>Username: {user.username} <input type='button' value='Change'></input></div>
+      <div>Email: {user.email} <input type='button' value='Change'></input></div>
+      <div>Password: **** <input type='button' value='Change'></input></div>
     </main>
   );
 }
