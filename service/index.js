e801babedee7895756790be78e3ae222c16e9303
Adilyn Rose <arose45@byu.edu>
2026-03-10 14:04:33 -0600

    Added a register user function

--- service/index.js
+++ service/index.js
@@ -11,7 +11,51 @@ let users = [];
 const port = process.argv.length > 2 ? process.argv[2] : 3000;
 
 app.use(express.json());
+app.use(cookieParser());
+app.use(express.static('public'));
 
 let apiRouter = express.Router();
 app.use(`/api`, apiRouter);
 
+//Login
+async function registerUser(email, password, username) {
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = {
+        email: email,
+        password: hashedPassword,
+        username: username,
+        projects: [],
+        friends: [],
+        friendRequests: [],
+        token: uuid.v4(),
+    };
+
+    users.push(newUser);
+    return newUser;
+}
+
+apiRouter.post('/auth/create', async (req, res) => {
+  if (await findUser('email', req.body.email)) {
+    res.status(409).send({ msg: 'Existing user' });
+  } 
+  else if (await findUser('username', req.body.email)) {
+    res.status(409).send({ msg: 'Existing user' });
+  }
+  else {
+    const user = await registerUser(req.body.email, req.body.password, req.body.username);
+
+    setAuthCookie(res, user.token);
+    res.send({ currentUser: user.username });
+  }
+});
+
+async function findUser(field, value) {
+  if (!value) return null;
+
+  return users.find((u) => u[field] === value);
+}
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
--- src/projects/projectCode.jsx
+++ src/projects/projectCode.jsx
@@ -17,3 +17,17 @@
         <div id="Characters"><NavLink to="/character_sheets" id="fileLink">John</NavLink></div>
         <div id="Date">1/26/26</div>
       </div>*/}
+
+
+      const express = require('express');
+const app = express();
+
+const port = process.argv.length > 2 ? process.argv[2] : 3000;
+
+app.get('*', (_req, res) => {
+  res.send({ msg: 'Simon service' });
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
