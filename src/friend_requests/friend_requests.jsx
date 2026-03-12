7bf25513aead2401a1cb588d4e0472098960706e
Adilyn Rose <arose45@byu.edu>
2026-03-12 12:21:10 -0600

    friend_requests navigates to login when unauthorized

--- src/friend_requests/friend_requests.jsx
+++ src/friend_requests/friend_requests.jsx
@@ -1,22 +1,28 @@
 import React from 'react';
 import './friend_requests.css';
-import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, NavLink, useNavigate, Routes } from 'react-router-dom';
 import { Projects } from '../projects/projects';
 import { addFriend, removeRequest } from '../service';
 
 export function Friend_Requests() {
+    const navigate = useNavigate();
     const currentUser = localStorage.getItem('currentUser');
     /*const users = JSON.parse(localStorage.getItem('users'));
     const user = users.find((u) => u.username === currentUser.username);*/
     const [friendRequests, setFriendRequests] = React.useState([]);
 
     React.useEffect(() => {
-                fetch(`/api/friendRequests/${currentUser}`)
-                    .then((response) => response.json())
-                    .then((requests) => {
-                        setFriendRequests(requests);
-                    });
-            }, []);
+        fetch(`/api/friendRequests/${currentUser}`)
+            .then(async (response) => {
+                if (response?.status === 200) {
+                    const requestRes = await response.json();
+                    setFriendRequests(requestRes);
+                }
+                else if (response?.status === 401) {
+                    navigate('/');
+                }
+            })
+    }, []);
 
     async function accept(request) {
         const response = await fetch('/api/friends/add', {
@@ -37,16 +43,16 @@ export function Friend_Requests() {
         if (response2?.status === 200) {
             console.log(friendRequests);
             fetch(`/api/friendRequests/${currentUser}`)
-            .then((response) => response.json())
-            .then((requests) => {
-                setFriendRequests(requests);
+                .then((response) => response.json())
+                .then((requests) => {
+                    setFriendRequests(requests);
 
-            });
+                });
         } else {
             throw new Error('Failed to delete request');
         }
     }
-    
+
     async function decline(request) {
         const response = await fetch(`/api/friendRequests/${currentUser}/${request.id}`, {
             method: 'delete',
@@ -54,33 +60,33 @@ export function Friend_Requests() {
         if (response?.status === 200) {
             console.log(friendRequests);
             fetch(`/api/friendRequests/${currentUser}`)
-            .then((response) => response.json())
-            .then((requests) => {
-                setFriendRequests(requests);
+                .then((response) => response.json())
+                .then((requests) => {
+                    setFriendRequests(requests);
 
-            });
+                });
         } else {
             throw new Error('Failed to delete request');
         }
     }
 
     return (
-    <main>
-        <NavLink to='/projects' id="fileLink">Back to Projects</NavLink>
-        <p></p>
-        <div><NavLink to='/friends' id='fileLink'>Friends</NavLink> | <b>Requests</b></div>
-        <div id="requestOrganizer">
-            <div id="requestName"><b>Name</b></div>
-            <div id="requestDate"><b>Date</b></div>
-        </div>
-        {friendRequests.map(request => (
-                    <div key={request.id} id='requestOrganizer'>
-                        <div id="requestName">{request.from}</div>
-                        <div id="requestDate">{request.time}</div>
-                        <input type='button' value='Accept' onClick={() => accept(request)}/>
-                        <input type='button' value='Decline' onClick={() => decline(request)}/>
-                    </div>
-                ))}
-    </main>
-  );
+        <main>
+            <NavLink to='/projects' id="fileLink">Back to Projects</NavLink>
+            <p></p>
+            <div><NavLink to='/friends' id='fileLink'>Friends</NavLink> | <b>Requests</b></div>
+            <div id="requestOrganizer">
+                <div id="requestName"><b>Name</b></div>
+                <div id="requestDate"><b>Date</b></div>
+            </div>
+            {friendRequests.map(request => (
+                <div key={request.id} id='requestOrganizer'>
+                    <div id="requestName">{request.from}</div>
+                    <div id="requestDate">{request.time}</div>
+                    <input type='button' value='Accept' onClick={() => accept(request)} />
+                    <input type='button' value='Decline' onClick={() => decline(request)} />
+                </div>
+            ))}
+        </main>
+    );
 }
